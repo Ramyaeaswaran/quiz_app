1c4bbeef31f3fb921058c2c54a750c1bc19869be
Ramyaeaswaran <ramyaeaswaran77@gmail.com>
2019-10-04 15:48:42 +0530

    quiz_app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: QuizApp(),
-      ),
-    ),
+     QuizApp(),
   );
 }
 
@@ -18,8 +14,44 @@ class QuizApp extends StatefulWidget {
 class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Text("Your content here"),
+    return MaterialApp(
+        home: Scaffold(
+        backgroundColor: Colors.black,
+          body: Center(
+             child: Column(
+               crossAxisAlignment: CrossAxisAlignment.stretch,
+               children: <Widget>[
+               Expanded(flex: 5,
+                   child: Padding(
+                     padding: const EdgeInsets.only(top:200),
+                     child: Text("This is where the question text will go",
+                       textAlign: TextAlign.center,
+                       style: TextStyle(color: Colors.white,fontSize: 20),),
+                   )),
+                 Expanded(flex: 1,
+                   child: Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: FlatButton(
+                       child:Text('True',style: TextStyle(color: Colors.white),),
+                         color: Colors.green,
+                       onPressed: (){},
+                     ),
+                   ),
+                 ),
+                   Expanded(flex: 1,
+                     child: Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: FlatButton(
+                         child:Text('False',style: TextStyle(color: Colors.white),),
+                         color: Colors.red,
+                         onPressed: (){},
+                       ),
+                     ),
+                   ),
+        ],
+      ),
+    ),
+        ),
     );
   }
 }
